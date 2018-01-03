0504ad39d02ea4ac9864e2b4c0e1985cafda0e58
Ross Fletcher <rtbfletch@outlook.com>
2018-01-02 23:02:48 -0800

    add tests for first three BDD specs, all failed, all passed

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -7,6 +7,13 @@ public class PingPong {
 
     public ArrayList<Object> runPingPong(int countUpTo){
         ArrayList<Object> result = new ArrayList<Object>();
-        return null;
+        for (int i = 1; i <= countUpTo; i++){
+            if (i % 3 == 0){
+                result.add("ping");
+            } else {
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
--- src/test/java/models/PingPongTest.java
+++ src/test/java/models/PingPongTest.java
@@ -16,4 +16,23 @@ public class PingPongTest {
         assertEquals(expectedOutput, testPingPong.runPingPong(1));
     }
 
+    @Test
+    public void runPingPong_countUpToGivenNumber_ArrayList() throws Exception {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        assertEquals(expectedOutput, testPingPong.runPingPong(2));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf3_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        assertEquals(expectedOutput, testPingPong.runPingPong(3));
+    }
+
 }
